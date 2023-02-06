cfcef78270df84ca566e18afb80773c98617fcc4
nicholas-myers <nicholas.myers.professional@gmail.com>
2023-02-05 18:41:38 -0800

    add conditionals for nav bar based on logged in status

--- src/views/components/Header.tsx
+++ src/views/components/Header.tsx
@@ -1,30 +1,41 @@
+import { useState } from "react";
 import { Button, Nav, NavItem } from "reactstrap";
 import { Link } from "react-router-dom";
+
 const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <header className="d-flex justify-content-between p-3">
       <h1>Site Name</h1>
       <Nav className="w-50 justify-content-around">
-        <NavItem>
-          <Link to="signup">
-            <Button>Signup</Button>
-          </Link>
-        </NavItem>
-        <NavItem>
-          <Link to="login">
-            <Button>Login</Button>
-          </Link>
-        </NavItem>
-        <NavItem>
-          <Link to="/">
-            <Button>Logout</Button>
-          </Link>
-        </NavItem>
-        <NavItem>
-          <Link to="profile">
-            <Button>Profile</Button>
-          </Link>
-        </NavItem>
+        {isLoggedIn === false && (
+          <>
+            <NavItem>
+              <Link to="signup">
+                <Button>Signup</Button>
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link to="login">
+                <Button>Login</Button>
+              </Link>
+            </NavItem>
+          </>
+        )}
+        {isLoggedIn === true && (
+          <>
+            <NavItem>
+              <Link to="/">
+                <Button>Logout</Button>
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link to="profile">
+                <Button>Profile</Button>
+              </Link>
+            </NavItem>
+          </>
+        )}
       </Nav>
     </header>
   );
